598072a6769a68a77418bde486b516d52215f68e
YanekaraAbe <keisuke.abe@yanekara.jp>
2025-09-22 17:10:42 +0900

    add test for esp32dev

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -10,3 +10,14 @@ int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
+
+#ifdef ESP32_DEV
+#include <Arduino.h>
+void setup() {
+  Serial.begin(115200);
+  ::testing::InitGoogleTest();
+  RUN_ALL_TESTS();
+}
+
+void loop() {}
+#endif
